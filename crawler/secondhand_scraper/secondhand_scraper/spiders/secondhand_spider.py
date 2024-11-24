982ca5478e69cafc35f3a455108bca9c310118d9
Fireblast9 <sasha.toscano97@gmail.com>
2024-11-24 20:31:27 +0100

    First crawl succeeded

--- crawler/secondhand_scraper/secondhand_scraper/spiders/secondhand_spider.py
+++ crawler/secondhand_scraper/secondhand_scraper/spiders/secondhand_spider.py
@@ -11,13 +11,10 @@ class SecondHandSpider(scrapy.Spider):
 
         # Debug: Print the number of links found
         self.log(f"Found {len(links)} links on {response.url}")
-
-        # # Debug: Print the number of links found
-        # self.log(f"Found {len(links)} links on {response.url}")
         
-        # for link in links:
-        #     # Ensure the link is absolute
-        #     if link.startswith("/"):
-        #         link = response.urljoin(link)
+        for link in links:
+            # Ensure the link is absolute
+            if link.startswith("/"):
+                link = response.urljoin(link)
 
-        #     yield {'link': link}
+            yield {'link': link}
