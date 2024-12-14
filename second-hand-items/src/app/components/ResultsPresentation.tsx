19f08aa20691b483f27900f45d943447ed25991f
Matteo Ghilardini <matteo.ghilardini@gmail.com>
2024-12-14 10:10:00 +0100

    Enhance ResultsPresentation to display documents by category using Accordion components

--- second-hand-items/src/app/components/ResultsPresentation.tsx
+++ second-hand-items/src/app/components/ResultsPresentation.tsx
@@ -1,4 +1,6 @@
 import { Document } from '@/store/models';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import DocumentPresentation from './DocumentPresentation';
 
@@ -9,13 +11,43 @@ const ResultsPresentation = () => {
     (state: any) => state.documents.resultDocuments
   );
 
+  const categories = useSelector((state: any) => state.documents.categories);
+
+  const [resultsData, setResultsData] = useState<any>([]);
+
+  useEffect(() => {
+    const results = categories.map((category: string) => ({
+      category,
+      count: documents.filter((document) => document.category === category)
+        .length,
+    }));
+    setResultsData(results);
+  }, [documents]);
+
   return (
     <div>
-      {documents.map((doc) => (
-        <div key={doc.docno}>
-          <DocumentPresentation document={doc} />
-        </div>
-      ))}
+      {resultsData
+        .filter((result: any) => result.count > 0)
+        .map((result: any) => (
+          <div key={result.category}>
+            <Accordion>
+              <AccordionSummary>
+                {result.category}: {result.count} results
+              </AccordionSummary>
+
+              <AccordionDetails>
+                {documents
+                  .filter((document) => document.category === result.category)
+                  .map((document) => (
+                    <DocumentPresentation
+                      key={document.docno}
+                      document={document}
+                    />
+                  ))}
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        ))}
     </div>
   );
 };
