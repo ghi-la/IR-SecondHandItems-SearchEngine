ccfc1376f082042392d171ec35ba8ddb3a1bd45d
Matteo Ghilardini <matteo.ghilardini@gmail.com>
2024-12-17 15:51:33 +0100

    Add Material-UI theme provider and create custom theme for consistent styling

--- second-hand-items/src/app/appWrapper.tsx
+++ second-hand-items/src/app/appWrapper.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import store from '@/store/store';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import LoadingBackdrop from './components/LoadingBackdrop';
 import Notification from './components/Notification';
+import theme from './style/theme';
 
 export default function AppWrapper({
   children,
@@ -12,9 +15,12 @@ export default function AppWrapper({
 }>) {
   return (
     <Provider store={store}>
-      {children}
-      <Notification />
-      <LoadingBackdrop />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+        <Notification />
+        <LoadingBackdrop />
+      </ThemeProvider>
     </Provider>
   );
 }
